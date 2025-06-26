2e8d7650e25fb3a3b49feec9900b944a3012009a
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-06-26 18:42:42 +0530

    test.py

--- test.py
+++ test.py
@@ -1023,5 +1023,6 @@ def certificate():
     return render_template("cert.html", name=name, course=course, date=date)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
     scheduler.start()
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
