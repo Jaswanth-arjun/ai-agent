087e750d098777a76397c64978f583fabd80f1ce
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-10-02 09:29:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1001,5 +1001,6 @@ def certificate():
 
 if __name__ == "__main__":
     scheduler.start()
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=False, host='0.0.0.0', port=8000, use_reloader=False)
+
 
